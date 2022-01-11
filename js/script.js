3d80406c0f929dfff1290a984bec2c630ba7e9ac
AdamPillis <adam.pillis@hotmail.com>
2022-01-11 13:41:17 +0000

    Add: use 'this' method

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
- 
-
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-       $(".stream2").removeClass('highlight_stream');
-       $(".stream3").removeClass('highlight_stream');
-         $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
-       $(".stream1").removeClass('highlight_stream');
-       $(".stream2").removeClass('highlight_stream');
-       $(".stream3").removeClass('highlight_stream');
-         $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
-       $(".stream1").removeClass('highlight_stream');
-       $(".stream2").removeClass('highlight_stream');
-       $(".stream3").removeClass('highlight_stream');
-         $(".stream3").addClass('highlight_stream');
-   });
-
-
-}); 
+    $('.box').on('click', function() {
+      var classNames = $(this).attr('class').split(' ');
+      var boxClass = classNames[0];
+      var className = classNames[1];
+      if ($(this).css('background-color') == 'rgb(255, 0, 0') {
+        // if object already red, turn it black
+        $('.' + className).css('background-color', '#000');
+      } else {
+        //else turn all elements with a box class black
+        // and then change color of all elements
+        // with the same class name as the clicked element
+        // to red.
+        $('.' + boxClass).css('background-color', '#000');
+        $('.' + className).css('background-color', 'red');
+      }
+      
+    });
+});
